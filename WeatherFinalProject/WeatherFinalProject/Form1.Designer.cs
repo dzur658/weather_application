9a4ed5707a6cc1b90d1a34528a48a0e8c3baf886
dzur658 <dzureca7@gmail.com>
2018-04-17 20:43:52 -0400

    working pannel
    pannels 100% functional without text

--- WeatherFinalProject/WeatherFinalProject/Form1.Designer.cs
+++ WeatherFinalProject/WeatherFinalProject/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(824, 553);
+            this.ClientSize = new System.Drawing.Size(884, 761);
             this.Name = "Form1";
             this.Text = "WeatherForecastApplication";
             this.Load += new System.EventHandler(this.Form1_Load);
